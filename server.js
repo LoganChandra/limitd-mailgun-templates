1ca886f30b5282116a2ac1648c6910a6c20cc979
Logan <loganjchandra@gmail.com>
2021-01-11 14:14:06 +0800

    converted tailwind into inline css

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3001
 app.set('view engine', 'pug')
 app.use(express.static('img'));
 app.get('/', (req, res) => {
-    res.render('askIsLive.pug', {
+    res.render('sellerVerificationFail.pug', {
         shoe_image: "http://stockx.imgix.net/images/Adidas-Yeezy-Boost-350-V2-Zebra-Product-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321670",
         shoe_name: 'adidas Yeezy Boost 350 V2 Zebra',
         order_number: 12345678,
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
         processing_fee: 20,
         authentication_fee: 10,
         shipping_fee: 20,
-        total: 850,
+        total: 850, 
         button_link: '#',
         sender_address: 'Subang Jaya',
         unsubscribe_link: '#',
