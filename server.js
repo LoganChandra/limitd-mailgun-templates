230505368ffefeb04b8e10619336a6264c59a3a6
Logan <loganjchandra@gmail.com>
2021-01-22 16:59:32 +0800

    fixed calculation table

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const Email = require('email-templates');
+
 const app = express()
 const port = 3001
 
 app.set('view engine', 'pug')
 app.use(express.static('img'));
+
 app.get('/', (req, res) => {
-    res.render('sellerVerificationFail.pug', {
+  
+    res.render('askIsLive.pug', {
         shoe_image: "http://stockx.imgix.net/images/Adidas-Yeezy-Boost-350-V2-Zebra-Product-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321670",
         shoe_name: 'adidas Yeezy Boost 350 V2 Zebra',
         order_number: 12345678,
