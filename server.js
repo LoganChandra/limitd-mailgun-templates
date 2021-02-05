99b12126790dc2712d6909dcf4a269c66f19e74b
Logan <loganjchandra@gmail.com>
2021-02-05 12:54:56 +0800

    ui fixes added flow_type variable

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('img'));
 
 app.get('/', (req, res) => {
   
-    res.render('orderPlaced_SHIP.pug', {
+    res.render('bidSuccessful.pug', {
         shoe_image: "http://stockx.imgix.net/images/Adidas-Yeezy-Boost-350-V2-Zebra-Product-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321670",
         shoe_name: 'adidas Yeezy Boost 350 V2 Zebra',
         order_number: 12345678,
@@ -24,7 +24,8 @@ app.get('/', (req, res) => {
         unsubscribe_link: '#',
         facebook_link : '#',
         insta_link : '#',
-        twitter_link : '#'
+        twitter_link : '#',
+        flow_type: 'Bid'
     })
 })
 
