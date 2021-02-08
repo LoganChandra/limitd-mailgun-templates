d16fee4b206006116319b8acae9b37cb95540b40
Logan <loganjchandra@gmail.com>
2021-02-08 19:01:00 +0800

    ui fixes/removed flow_type

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('img'));
 
 app.get('/', (req, res) => {
   
-    res.render('bidSuccessful.pug', {
+    res.render('orderPlaced_SOLD.pug', {
         shoe_image: "http://stockx.imgix.net/images/Adidas-Yeezy-Boost-350-V2-Zebra-Product-1.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606321670",
         shoe_name: 'adidas Yeezy Boost 350 V2 Zebra',
         order_number: 12345678,
@@ -18,9 +18,9 @@ app.get('/', (req, res) => {
         processing_fee: 20,
         authentication_fee: 10,
         shipping_fee: 20,
-        total: 850, 
-        button_link: '#',
-        sender_address: 'Subang Jaya',
+        total: 850,
+        button_link: 'https://google.com',
+        sender_address: 'A-2-1, No. 36G Jalan Tanjung SD13/2, Bandar Sri Damansara, 52200 , Kuala Lumpur',
         unsubscribe_link: '#',
         facebook_link : '#',
         insta_link : '#',
